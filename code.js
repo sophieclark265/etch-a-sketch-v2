fc32e5f91c291a2fdb0c981e1c5d5f552cf447e8
Sophie <sophieclark526@gmail.com>
2022-07-01 15:24:13 +0200

    Got input grid size + replace working

--- code.js
+++ code.js
@@ -3,15 +3,17 @@ document.body.onload = loadEtchASketch;
 let containerDiv = document.getElementById("containerDiv");
 
 function loadEtchASketch() {
-  function addDivByJs(initialNumberOfDivs) {
+  function addDivByJs(initialNumberOfDivs, holdSquares) {
     console.log("addDivByJs func just got called");
+    console.log(containerDiv.innerHTML);
+
     let numberOfDivsToBeCreated = initialNumberOfDivs * initialNumberOfDivs;
     let childDiv = document.createDocumentFragment;
+
     for (let i = 0; i < numberOfDivsToBeCreated; i++) {
       let cell = document.createElement("div");
       cell.classList.add("small-square");
-
-      containerDiv.appendChild(cell);
+      holdSquares.appendChild(cell);
       cell.style.border = "1px solid black";
     }
   }
@@ -38,19 +40,32 @@ function loadEtchASketch() {
     btn.addEventListener("click", makingCustomGrid);
   }
 
+  // Deletes all squares and creates a new container for other squares
+  // returns the new container for other squares
+  function replaceSquares(containerDiv) {
+    console.log(containerDiv);
+    let holdSquares = containerDiv.children[0];
+    holdSquares.remove();
+    console.log("removing holds squares");
+    let newHoldSquares = document.createElement("div");
+    newHoldSquares.id = "holds-squares";
+    containerDiv.appendChild(newHoldSquares);
+    return newHoldSquares;
+  }
+
   function makingCustomGrid() {
     let userInput = prompt("How many squares do you want your grid to be?");
+    let holdSquares = containerDiv.children[0];
     if (userInput > 100) {
       prompt("whoops! Please try again");
     } else {
-      containerDiv.remove();
-      console.log("removing container div");
-      addDivByJs(userInput);
+      const newHoldSquares = replaceSquares(containerDiv);
+      addDivByJs(userInput, newHoldSquares);
     }
   }
 
   let initialNumberOfDivs = 16;
-  addDivByJs(initialNumberOfDivs);
+  addDivByJs(initialNumberOfDivs, containerDiv.children[0]);
   highlightSquares();
   requestCustomGrid();
 }
